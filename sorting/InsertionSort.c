0c2939a453009eb3844c8c15f50057214f3163ac
Rohan Verma <rohanvermaara@gmail.com>
2022-03-01 08:37:15 +0530

    added InsertionSort

---
+++ sorting/InsertionSort.c
@@ -0,0 +1,38 @@
+/**
+ * @file InsertionSort.c
+ * @author Rohan Verma (rohanvermaara@gmail.com)
+ * @brief Time Complexity: O(n^2)
+ * @version 0.1
+ * @date 2022-02-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+#include "Sorting.h"
+#define ELEMENTS 669, 71, 130, 87, 204, 755, 40, 503, 923, 922
+#define SIZE 10
+
+void printArray(const int * arr, int size);
+
+int main() {
+    int arr[] = {ELEMENTS};
+
+    printf("Before Sorting: ");
+    printArray(arr, SIZE);
+    printf("\nAfter Sorting:  ");
+    InsertionSort(arr, SIZE);
+    printArray(arr, SIZE);
+    printf("\n");
+
+    return 0;
+}
+
+void printArray(const int * arr, int size) {
+    printf("{");
+    for(int i=0; i<size-1; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("%d}", arr[size-1]);
+}
--- sorting/Sorting.c
+++ sorting/Sorting.c
@@ -24,7 +24,15 @@ void BubbleSort(int * arr, int size) {
 }
 
 void InsertionSort(int * arr, int size) {
-
+    for(int i=1; i<size; i++) {
+        int j=i-1;
+        int key=arr[i];
+        while(j>=0 && arr[j]>key) {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
 }
 
 
